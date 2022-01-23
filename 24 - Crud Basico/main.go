049c1109b90597d5e7d7a31fd938654911361e96
Joao Victor Magalhaes <joao.jmagalhaes@mercadolivre.com>
2022-01-22 22:59:30 -0300

    Get one and update

--- 24 - Crud Basico/main.go
+++ 24 - Crud Basico/main.go
@@ -14,6 +14,7 @@ func main() {
 	router.HandleFunc("/user", server.CreateUser).Methods(http.MethodPost)
 	router.HandleFunc("/user", server.GetUsers).Methods(http.MethodGet)
 	router.HandleFunc("/user/{id}", server.GetUser).Methods(http.MethodGet)
+	router.HandleFunc("/user/{id}", server.UpdateUser).Methods(http.MethodPut)
 
 	log.Fatalln(http.ListenAndServe(":8080", router))
 
--- 24 - Crud Basico/server/repository.go
+++ 24 - Crud Basico/server/repository.go
@@ -3,9 +3,11 @@ package server
 import (
 	"crud/database"
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type user struct {
@@ -100,5 +102,105 @@ func GetUsers(writter http.ResponseWriter, request *http.Request) {
 }
 
 func GetUser(writter http.ResponseWriter, request *http.Request) {
+	params := mux.Vars(request)
 
+	ID, err := strconv.ParseUint(params["id"], 10, 32)
+
+	if err != nil {
+		writter.WriteHeader(http.StatusBadRequest)
+		writter.Write([]byte("Error parsing id"))
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		writter.WriteHeader(http.StatusInternalServerError)
+		log.Println("Failed to connect in db", err)
+		return
+	}
+	defer db.Close()
+
+	row, err := db.Query("SELECT * FROM person WHERE id = $1", ID)
+	if err != nil {
+		writter.WriteHeader(http.StatusInternalServerError)
+		log.Println("Failed to query user", err)
+		return
+	}
+
+	var user user
+	if row.Next() {
+		if err := row.Scan(&user.ID, &user.Name, &user.Email); err != nil {
+			writter.WriteHeader(http.StatusInternalServerError)
+			log.Println("Failed to scan user", err)
+			return
+		}
+	}
+
+	if user.ID == 0 {
+		writter.WriteHeader(http.StatusNotFound)
+		writter.Write([]byte("User not found"))
+		return
+	}
+
+	writter.Header().Set("Content-Type", "application/json")
+	writter.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(writter).Encode(user); err != nil {
+		writter.WriteHeader(http.StatusInternalServerError)
+		log.Println("Failed to parse response", err)
+		return
+	}
+
+}
+
+func UpdateUser(writter http.ResponseWriter, request *http.Request) {
+	params := mux.Vars(request)
+
+	ID, err := strconv.ParseUint(params["id"], 10, 32)
+
+	if err != nil {
+		writter.WriteHeader(http.StatusBadRequest)
+		writter.Write([]byte("Error parsing id"))
+		return
+	}
+
+	body, err := ioutil.ReadAll(request.Body)
+
+	if err != nil {
+		writter.WriteHeader(http.StatusBadRequest)
+		writter.Write([]byte("Error reading body"))
+		return
+	}
+
+	var userRequest user
+	if err = json.Unmarshal(body, &userRequest); err != nil {
+		writter.WriteHeader(http.StatusBadRequest)
+		writter.Write([]byte("Error parsing body"))
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		writter.WriteHeader(http.StatusInternalServerError)
+		log.Println("Failed to connect in db", err)
+		return
+	}
+	defer db.Close()
+
+	var response user
+
+	if err = db.QueryRow("UPDATE person SET name = $1, email = $2 WHERE id = $3 RETURNING id, name, email",
+		userRequest.Name, userRequest.Email, ID).Scan(&response.ID, &response.Name, &response.Email); err != nil {
+		writter.WriteHeader(http.StatusInternalServerError)
+		log.Println("Failed to update user", err)
+
+		return
+	}
+
+	writter.Header().Set("Content-Type", "application/json")
+	writter.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(writter).Encode(response); err != nil {
+		writter.WriteHeader(http.StatusInternalServerError)
+		log.Println("Failed to parse response", err)
+		return
+	}
 }
